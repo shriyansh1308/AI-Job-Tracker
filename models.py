eaade7a7defec19daee39b34a42c03371c601001
shriyansh1308 <shriyansh1308@gmail.com>
2026-03-31 12:13:36 +0530

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Job(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    company = db.Column(db.String(100))
-    role = db.Column(db.String(100))
-    status = db.Column(db.String(50))
